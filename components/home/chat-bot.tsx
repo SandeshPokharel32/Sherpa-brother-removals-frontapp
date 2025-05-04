c5f94e4a0c7195829e3537440968cbb71b7244fe
Sandesh Pokharel <saandesh32@gmail.com>
2025-05-05 01:10:36 +0545

    whatsapp

--- components/home/chat-bot.tsx
+++ components/home/chat-bot.tsx
@@ -7,11 +7,11 @@ import "react-whatsapp-widget/dist/index.css";
 const ChatBot = () => {
   return (
     <div className="z-[200]">
-      {/* <FloatingWhatsApp
-        phoneNumber="+9779860609417"
-        accountName="Xtreme Adventure Helpline"
-        chatMessage={`Welcome to Xtreme Adventure!Ready to take your thrill-seeking ?`}
-      /> */}
+      <FloatingWhatsApp
+        phoneNumber="+977 985‑1235820"
+        accountName="Extreme Adventure Helpline"
+        chatMessage={`Welcome to Extreme Adventure! Ready to take your thrill-seeking ?`}
+      />
     </div>
   );
 };
